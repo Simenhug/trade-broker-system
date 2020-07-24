ad86605670f8d6956851e12eede60796df5d00ff
Simen Huang <xinmianhug@gmail.com>
2020-07-23 23:49:58 -0400

    added ecosystem.config.js to support PM2

---
+++ src/main/resources/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'trade-simulator',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-172-230-51.compute-1.amazonaws.com',
+      key: '/c/Users/simen/.ssh/trade_simulator/AWSfreet2microTradeSimulator.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:Simenhug/trade-broker-system.git',
+      path: '/home/ubuntu/Codes/trade_simulator',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
